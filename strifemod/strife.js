a605a7a37778ea5a13b7d9c79813c376b36d2bd9
5xdf <89792698+5xdf@users.noreply.github.com>
2024-04-03 14:35:15 +0000

    IDK

--- strifemod/strife.js
+++ strifemod/strife.js
@@ -4170,7 +4170,7 @@
     await ReactNative.Image.prefetch("https://bound-mod.github.io/assets/images/fools.png");
     logger_default.log("Strife has been injected into your discord app successfully!");
     showToast("Strife Loaded", getAssetIDByName("toast_copy_link"));
-    window.alert("You are on the BETA branch of Strife! By using this branch, you accept any data leakage, broken features, and any other possible risks. I am not responsible for these risks. You decided to turn on the beta branch switch in settings, so you are having to deal with this. Turn off the beta branch to minimize the risks.");
+    // window.alert("You are on the BETA branch of Strife! By using this branch, you accept any data leakage, broken features, and any other possible risks. I am not responsible for these risks. You decided to turn on the beta branch switch in settings, so you are having to deal with this. Turn off the beta branch to minimize the risks.");
   }
   var init_src = __esm({
     "src/index.ts"() {
